6fe778c8f201cee74fa441c17314c010e7416597
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2023-03-16 17:56:20 +0000

    Att settigs layout

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -15,11 +15,19 @@ export default function Settings() {
 
   return (
     <div className="pt-20">
-      <div> {/* Div Feed  */}
-      <h1>Teste</h1>
+      <div className="ml-10"> {/* Div Feed  */}
+        <header className=" text-black text-xl font-bold ">Feed</header>
+        <h1 className=" ml-2 p-1">DeFi Junk shitposters filter</h1>
+        <h1 className=" ml-2 p-1">Exploring on weekends</h1>
+        <h1 className=" ml-2 p-1">Suggest opposite view content</h1>
+        <h1 className=" ml-2 p-1">Engagement bait Filter</h1>
       </div>
-      <div> {/* Div  Connection */}
-      <h1>Teste 2</h1>
+      <div className=" ml-10 p-4"> {/* Div  Connection */}
+      <header className=" text-black text-xl font-bold ">Connections</header>
+        <h1 className=" ml-2 p-1">DeFi Junk shitposters filter</h1>
+        <h1 className=" ml-2 p-1">Exploring on weekends</h1>
+        <h1 className=" ml-2 p-1">Suggest opposite view content</h1>
+        <h1 className=" ml-2 p-1">Engagement bait Filter</h1>
       </div>
       <div> {/* Div Parameters  */}
       </div>
