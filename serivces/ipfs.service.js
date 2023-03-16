fac5c83236f2467fe4a1f1531e3cbc47f1776995
Alexander Kozlov <itrefak@gmail.com>
2023-03-16 20:54:22 +0000

    brushing up services

---
+++ serivces/ipfs.service.js
@@ -0,0 +1,12 @@
+
+export class IPFSService {
+    async storeJSON(jsonStr) {
+        // return hash;
+        return 'sadfasdfasdfasdfasdfasdf'
+    }
+
+    async constructURL(hash) {
+        // made up hash
+        return `ipfs://infure.com/${hash}`;
+    }
+}
