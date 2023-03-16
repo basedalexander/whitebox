231d20fe36e67ea166164a44e8bc9323fb1e808b
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2023-03-16 16:42:09 +0000

    Att Index.tsx is layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,25 +89,46 @@ export default function Home() {
   }
 
   return (
+    <div className="pt-20">
+      <h1 className="text-5xl mb-6 font-bold flex flex-col justify-center items-center ">
+        Hello Lens 🌿
+      </h1>
+      <div className="flex flex-col justify-center items-center ">
+      <input
+        type="text"
+        placeholder="Digite algo aqui"
+        className="border-teal-700 bg-gray-300 w-96 rounded"
+        style={{ paddingLeft: '10px', paddingRight: '20px' }}
+      />
+      </div>
 
-    <div className='pt-20'>
-
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className='text-5xl mb-6 font-bold'>Hello Lens 🌿</h1>
-        {
-          profiles.map(profile => (
-            <div key={profile.id} className='w-2/3 shadow-md p-6 rounded-lg mb-8 flex flex-col items-center'>
-              <img className='w-48' src={profile.avatarUrl || 'https://picsum.photos/200'} />
-              <p className='text-xl text-center mt-6'>{profile.name}</p>
-              <p className='text-base text-gray-400  text-center mt-2'>{profile.bio}</p>
-              <Link href={`/profile/${profile.handle}`}>
-                <p className='cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2'>{profile.handle}</p>
-              </Link>
-              <p className='text-pink-600 text-sm font-medium text-center'>{profile.stats.totalFollowers} followers</p>
+      <div className="flex flex-col items-center justify-cente w-2/5 mx-auto mt-10 bg-gray-400 rounded-xl ">
+        {profiles.map((profile) => (
+          <div
+            key={profile.id}
+            className="flex flex-col items-center justify-center mt-10 bg-white rounded-xl w-2/3"
+          >
+            <div className="flex items-center w-full">
+              <img
+                className="w-10 h-10 rounded-full ml-5"
+                src={profile.avatarUrl || "https://picsum.photos/200"}
+              />
+              <div className="w-full ml-5">
+                <p className="text-xl font-bold">{profile.name}</p>
+                <p className="text-base text-gray-400">{profile.bio}</p>
+              </div>
             </div>
-          ))
-        }
+            <Link href={`/profile/${profile.handle}`}>
+              <p className="cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2">
+                {profile.handle}
+              </p>
+            </Link>
+            <p className="text-pink-600 text-sm font-medium text-center">
+              {profile.stats.totalFollowers} Followers
+            </p>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,22 +1,29 @@
 /* app/page.tsx */
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { client, exploreProfiles } from '../api'
-import Link from 'next/link'
+import { useEffect, useState } from "react";
+import { client, exploreProfiles } from "../api";
+import Link from "next/link";
 
 export default function Settings() {
   /* create initial state to hold array of profiles */
-  const [profiles, setProfiles] = useState<any>([])
+  const [profiles, setProfiles] = useState<any>([]);
   useEffect(() => {
-    fetchProfiles()
-  }, [])
-  async function fetchProfiles() {
-    
-  }
+    fetchProfiles();
+  }, []);
+  async function fetchProfiles() {}
+
   return (
-    <div className='pt-20'>
+    <div className="pt-20">
+      <div> {/* Div Feed  */}
+      </div>
+      <div> {/* Div  Connection */}
+      </div>
+      <div> {/* Div Parameters  */}
+      </div>
+      <div> {/* Div Code  */}
+      </div>
       Settings page
     </div>
-  )
+  );
 }
