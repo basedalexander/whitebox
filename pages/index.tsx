e8a8c9004dee1fe93e433561b679e23973b892cf
Alexander Kozlov <itrefak@gmail.com>
2023-03-17 13:00:06 +0000

    feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { client, exploreProfiles, authenticate, challenge, getProfile, getPublications } from '../api'
-import Link from 'next/link'
+import { gql, ApolloClient } from '@apollo/client'
+import { client, exploreProfiles, getProfile, getPublications } from '../api'
+import { replaceValues } from '@/services/utils/utils'
+
 
 export default function Home() {
 
@@ -16,16 +18,18 @@ export default function Home() {
 
   useEffect(() => {
     init();
-    fetchProfiles();
   }, [])
 
   async function init() {
+    // 1. Download all algos
+    // 2. Get Applied algos
+    // 3. Query the feed using algorithms 
     await refreshFeed();
   }
 
   async function refreshFeed() {
       const profileData = await fetchProfile(handle);
-      const publications: any = await getPubs(profileData);
+      const publications: any = await fetchPubs(profileData);
       const feedItems = publications.data.publications.items;
       setPublications(feedItems);
       console.log('feed refreshed');
@@ -50,12 +54,29 @@ export default function Home() {
     
     console.log(`profile ${handle} fetched`);
     console.log(profileData);
-    
 
     return profileData;
   }
 
-  async function getPubs(profileData) {
+  async function buildQuery(code, params) {
+    const processedQueryStr = processQuery(code, params);
+    // return gql(processedQueryStr);
+  }
+  async function processQuery(code, params) {
+    const processedQuery = replaceValues(params, code);
+    return processedQuery;
+  }
+
+  async function QueryFeed(query) {
+  //   const queryExample = async () => {
+  //     const response = await apolloClient.query({
+  //      query: gql(query),
+  //    })
+  //    console.log('Lens example data: ', response)
+  //  }
+  }
+
+  async function fetchPubs(profileData) {
     try {
       const result = await client.query({
         query: getPublications,
@@ -73,34 +94,8 @@ export default function Home() {
     }
   }
   
-  async function fetchProfiles() {
-    try {
-      /* fetch profiles from Lens API */
-      let response = await client.query({ query: exploreProfiles })
-      /* loop over profiles, create properly formatted ipfs image links */
-      let profileData = await Promise.all(response.data.exploreProfiles.items.map(async profileInfo => {
-        let profile = { ...profileInfo }
-        let picture = profile.picture
-        if (picture && picture.original && picture.original.url) {
-          if (picture.original.url.startsWith('ipfs://')) {
-            let result = picture.original.url.substring(7, picture.original.url.length)
-            profile.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
-          } else {
-            profile.avatarUrl = picture.original.url
-          }
-        }
-        return profile
-      }))
-
-      /* update the local state with the profiles array */
-      setProfiles(profileData)
-    } catch (err) {
-      console.log({ err })
-    }
-  }
 
   async function handleRefresh() {
-    await fetchProfiles();
     await refreshFeed();
     console.log('refresh executed');
   }
@@ -110,8 +105,8 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center ">
       <input
         type="text"
-        placeholder="Digite algo aqui!"
-        className="border-teal-700 bg-gray-300 w-96 rounded"
+        placeholder="What's new?"
+        className="border-teal-700 w-96 rounded"
         style={{ paddingLeft: '10px', paddingRight: '20px' }}
       />
       </div>
@@ -119,7 +114,7 @@ export default function Home() {
         <button onClick={handleRefresh}>REFRESH</button>
       </div>
 
-      <div className="flex flex-col items-center justify-cente w-2/5 mx-auto mt-10 bg-gray-400 rounded-xl ">
+      <div className="flex flex-col items-center justify-cente w-2/5 mx-auto mt-10  rounded-xl ">
       {
             publications.map(pub => (
               <div key={pub.id} className='shadow p-10 rounded mb-8 w-2/3'>
--- services/utils/utils.js
+++ services/utils/utils.js
@@ -1,3 +1,12 @@
 export function deepCopy(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
+
+export function replaceValues(obj, str) {
+  let newStr = str;
+  for (let key in obj) {
+    const regex = new RegExp(`{{${key}}}`, 'g');
+    newStr = newStr.replace(regex, obj[key]);
+  }
+  return newStr;
+}
