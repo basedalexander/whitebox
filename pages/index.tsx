276d7a344dc96fd0bd2a46b85cdad8ad7556d522
Alexander Kozlov <itrefak@gmail.com>
2023-03-17 11:16:28 +0000

    something

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,17 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { client, exploreProfiles, authenticate, challenge } from '../api'
+import { client, exploreProfiles, authenticate, challenge, getProfile, getPublications } from '../api'
 import Link from 'next/link'
 
 export default function Home() {
 
   /* create initial state to hold array of profiles */
   const [profiles, setProfiles] = useState<any>([])
+  const [publications, setPublications] = useState<any>([])
+    /* using the router we can get the lens handle from the route path */
+    const handle = 'stani.lens';
+
 
   useEffect(() => {
     init();
@@ -19,6 +23,49 @@ export default function Home() {
 
   }
 
+  async function refreshFeed() {
+      // const profileData = await fetchProfile(handle);
+      // const publications: any = await getPublications(profileData);
+      // const feedItems = publications.data.publications.items;
+      // setPublications(feedItems);
+      // console.log('feed refreshed');
+  }
+
+  async function fetchProfile(handle) {
+    const returnedProfile = await client.query({
+      query: getProfile,
+      variables: { handle }
+    })
+    const profileData = { ...returnedProfile.data.profile }
+    /* format their picture if it is not in the right format */
+    const picture = profileData.picture
+    if (picture && picture.original && picture.original.url) {
+      if (picture.original.url.startsWith('ipfs://')) {
+        let result = picture.original.url.substring(7, picture.original.url.length)
+        profileData.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
+      } else {
+        profileData.avatarUrl = profileData.picture.original.url
+      }
+    }
+
+    return profileData;
+  }
+
+  async function getPublications(profileData) {
+    try {
+      const result = await client.query({
+        query: getPublications,
+        variables: {
+            id: profileData.id, limit: 50
+        }
+      })
+
+      return result;
+    } catch(e) {
+      
+    }
+  }
+  
   async function fetchProfiles() {
     try {
       /* fetch profiles from Lens API */
@@ -47,6 +94,7 @@ export default function Home() {
 
   async function handleRefresh() {
     await fetchProfiles();
+    await refreshFeed();
     console.log('refresh executed');
   }
 
