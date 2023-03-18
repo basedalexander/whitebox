c5592482fff4a5086a0ca5743efc0b0448d30e16
Alexander Kozlov <itrefak@gmail.com>
2023-03-18 06:46:18 +0000

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,47 +63,6 @@ export default function Home() {
     return '0x8D60843A8B19c97375d1d67da1AC9049dDd807DC';
   }
 
-  // async function fetchProfile(handle) {
-  //   const returnedProfile = await client.query({
-  //     query: getProfile,
-  //     variables: { handle }
-  //   })
-  //   const profileData = { ...returnedProfile.data.profile }
-  //   /* format their picture if it is not in the right format */
-  //   const picture = profileData.picture
-  //   if (picture && picture.original && picture.original.url) {
-  //     if (picture.original.url.startsWith('ipfs://')) {
-  //       let result = picture.original.url.substring(7, picture.original.url.length)
-  //       profileData.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
-  //     } else {
-  //       profileData.avatarUrl = profileData.picture.original.url
-  //     }
-  //   }
-    
-  //   console.log(`profile ${handle} fetched`);
-  //   console.log(profileData);
-
-  //   return profileData;
-  // }
-  
-  // async function fetchPubs(profileData) {
-  //   try {
-  //     const result = await client.query({
-  //       query: getPublications,
-  //       variables: {
-  //           id: profileData.id, limit: 50
-  //       }
-  //     })
-
-  //     console.log('publications fetched');
-  //     console.log(result);
-
-  //     return result;
-  //   } catch(e) {
-  //     console.error(`error downloading publication for ${profileData.handle} \n ${e}`);
-  //   }
-  // }
-  
   async function handleRefresh() {
     await refreshFeed();
     console.log('refresh executed');
