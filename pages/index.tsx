42db7e87e37ee16c3a24e32f8bc27ad70627aa5b
Alexander Kozlov <itrefak@gmail.com>
2023-03-18 00:04:10 +0000

    added algo select on feed page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,47 @@
 /* app/page.tsx */
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { gql, ApolloClient } from '@apollo/client'
 import { client, exploreProfiles, getProfile, getPublications } from '../api'
 import { replaceValues } from '@/services/utils/utils'
+import { filterBrokenAlgos } from '../services/algos-utils'
+import useStoredAlgos from "@/services/useStoredAlgos";
+
+
 
 
 export default function Home() {
   const [profiles, setProfiles] = useState<any>([])
   const [publications, setPublications] = useState<any>([])
   const defaultHandle = 'stani.lens';
+  const [selectedAlgoIndex, setSelectedAlgoIndex] = useState<any>([]);
+
 
   useEffect(() => {
     init();
+    setSelectedAlgoIndex(0);
   }, [])
+
+  // ======= this mess is to fetch and transfortm algos data to render.
+  const [storedAlgos, algoHashes] = useStoredAlgos();
+  if (storedAlgos) {
+    console.log(storedAlgos);
+  }
+  
+  const processedAlgos = useMemo(() => {
+    storedAlgos?.forEach((algo, index) => {
+      algo.hash = algoHashes?.[index];
+    });
+    const res = filterBrokenAlgos(storedAlgos)
+    return res
+  }, [storedAlgos])
+
+  if (processedAlgos) {
+    console.log('processed algos');
+    console.log(processedAlgos)
+  }
+  // ===== mess ends
 
   async function init() {
     await refreshFeed();
@@ -50,19 +77,7 @@ export default function Home() {
 
     return profileData;
   }
-
-  async function buildQuery(code, params) {
-    const processedQueryStr = processQuery(code, params);
-    // return gql(processedQueryStr);
-  }
-  async function processQuery(code, params) {
-    const processedQuery = replaceValues(params, code);
-    return processedQuery;
-  }
-
-  async function QueryFeed(query) {
-  }
-
+  
   async function fetchPubs(profileData) {
     try {
       const result = await client.query({
@@ -81,34 +96,58 @@ export default function Home() {
     }
   }
   
-
   async function handleRefresh() {
     await refreshFeed();
     console.log('refresh executed');
   }
 
+  function onAlgoClick(index) {
+    setSelectedAlgoIndex(index);
+    console.log("algo click index ", index);
+  }
+
   return (
     <div className="pt-20">
-      <div className="flex flex-col justify-center items-center ">
-      <input
-        type="text"
-        placeholder="What's new?"
-        className="border-teal-700 w-96 rounded"
-        style={{ paddingLeft: '10px', paddingRight: '20px' }}
-      />
-      </div>
-      <div className="flex flex-col justify-center items-center ">
-        <button onClick={handleRefresh}>REFRESH</button>
+      <div>
+      <div className="ml-10 w-min">
+              <header className="text-black text-xl font-bold w-min">
+                Select Algo
+              </header>
+              <div>
+                {processedAlgos?.map((algo, index) => (
+                  <div key={algo.name} style={index === selectedAlgoIndex ? { color: 'blue' } : { color: 'black' }}
+                    onClick={() => onAlgoClick(index)}
+                    className="ml-2 p-1 hover:text-violet-700 truncate w-min"
+                  >
+                    {algo.name}
+                  </div>
+                ))}
+              </div>
+            </div>
       </div>
 
-      <div className="flex flex-col items-center justify-cente w-2/5 mx-auto mt-10  rounded-xl ">
-      {
-            publications.map(pub => (
-              <div key={pub.id} className='shadow p-10 rounded mb-8 w-2/3'>
-                <p>{pub.metadata.content}</p>
-              </div>
-            ))
-        }
+      <div>
+        <div className="flex flex-col justify-center items-center ">
+        <input
+          type="text"
+          placeholder="What's new?"
+          className="border-teal-700 w-96 rounded"
+          style={{ paddingLeft: '10px', paddingRight: '20px' }}
+        />
+        </div>
+        <div className="flex flex-col justify-center items-center ">
+          <button onClick={handleRefresh}>REFRESH</button>
+        </div>
+
+        <div className="flex flex-col items-center justify-cente w-2/5 mx-auto mt-10  rounded-xl ">
+        {
+              publications.map(pub => (
+                <div key={pub.id} className='shadow p-10 rounded mb-8 w-2/3'>
+                  <p>{pub.metadata.content}</p>
+                </div>
+              ))
+          }
+        </div>
       </div>
     </div>
   );
